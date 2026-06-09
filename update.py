cf99b3b6e4b2ff1d2d0bf9de26ec700cafe2ef66
Shyma Shameer <shymaazeez@gmail.com>
2026-06-09 10:12:33 +0200

    Update update.py

--- update.py
+++ update.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine, text
 import streamlit as st
 connection_string = st.secrets["connection_string"]
 
-#connection_string = 'mysql+pymysql://root:shymamysql@127.0.0.1:3306/liane_library'
+
 
 def update_friend(friend_id, first_name=None, last_name= None, email=None, phone=None,notes= None,max_loan= 2):
     engine = create_engine(connection_string)
